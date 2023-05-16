8a6d9528534a6c7f5a5e05fd8313ef00d5c6625d
NikolayDolnik <dolnik.nikolo@gmail.com>
2023-05-16 18:18:43 +0300

    sprint_8

---
+++ ImageeFeed/ImagesList/ImageListCell.swift
@@ -0,0 +1,17 @@
+//
+//  ImageListCell.swift
+//  ImageeFeed
+//
+//  Created by Dolnik Nikolay on 05.05.2023.
+//
+
+import UIKit
+
+class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+    
+    @IBOutlet var cellImage: UIImageView!
+    @IBOutlet var likeButton: UIButton!
+    @IBOutlet var dateLabel: UILabel!
+    
+}
